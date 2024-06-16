150b33273a59c6fce5d9f90aaba65456bb1fb495
mikefillar <mikefillar21@gmail.com>
2024-06-17 00:40:40 +0800

    file modified

--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -12,11 +12,11 @@ const Pages = () => {
     <Router>
       <Routes>
         <Route exact path="/car-rental-ph/" element={<Home />} />
-        <Route exact path="/car-rental-ph/about" element={<About />} />
-        <Route exact path="/car-rental-ph/vehicle" element={<Vehicle />} />
-        <Route exact path="/car-rental-ph/review" element={<Review />} />
-        <Route exact path="/car-rental-ph/team" element={<OurTeam />} />
-        <Route exact path="/car-rental-ph/contact" element={<Contact />} />
+        <Route path="/car-rental-ph/about" element={<About />} />
+        <Route path="/car-rental-ph/vehicle" element={<Vehicle />} />
+        <Route path="/car-rental-ph/review" element={<Review />} />
+        <Route path="/car-rental-ph/team" element={<OurTeam />} />
+        <Route path="/car-rental-ph/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
